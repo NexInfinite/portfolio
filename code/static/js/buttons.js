59b07e4f57fec2c4b40e4af02adf76979c3b08f1
Julian <julianjones663@gmail.com>
2023-02-14 21:02:54 +0000

    Finished back to top button

--- code/static/js/buttons.js
+++ code/static/js/buttons.js
@@ -2,3 +2,13 @@ function scrollToId(id) {
     let element = document.getElementById(id);
     element.scrollIntoView({ behavior: 'smooth', block: 'end'});
 }
+
+function showBackToTop() {
+    let backToTop = document.getElementById('back-to-top');
+    backToTop.hidden = false;
+}
+
+function hideBackToTop() {
+    let backToTop = document.getElementById('back-to-top');
+    backToTop.hidden = true;
+}
