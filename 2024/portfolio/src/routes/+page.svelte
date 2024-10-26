56242d66340005b9634639ec085c05fe84f3669d
Julian Jones <julianjones663@gmail.com>
2024-10-26 23:33:31 +0100

    fixed particles and chevron down

--- 2024/portfolio/src/routes/+page.svelte
+++ 2024/portfolio/src/routes/+page.svelte
@@ -1,20 +1,19 @@
 <script>
   import ParticlesComponent from "$lib/Particles.svelte";
+  import DownChevron from "$lib/DownChevron.svelte";
 </script>
 
 <ParticlesComponent />
 
-<div class="z-10 h-screen w-screen flex items-center justify-center">
+<div class="z-10 relative h-screen w-screen flex items-center justify-center">
   <div class="flex-col font-display text-center">
-    <div class="font-black text-primary text-3xl md:text-5xl">
+    <div class="font-black text-primary text-3xl md:text-5xl md:pb-2">
       WELCOME
     </div>
     <div class="font-thin text-main md:text-2xl">
       I'm Julian
     </div>
 
-    <div>
-      <i class="fa-solid fa-sort-down text-xl md:text-3xl"></i>
-    </div>
+    <DownChevron />
   </div>
 </div>
