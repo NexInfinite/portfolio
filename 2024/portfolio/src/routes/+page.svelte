c919d7c23d30efcc488546417c9592f306d9497b
Julian Jones <julianjones663@gmail.com>
2024-10-26 20:25:42 +0100

    added particles - they dont work

--- 2024/portfolio/src/routes/+page.svelte
+++ 2024/portfolio/src/routes/+page.svelte
@@ -1,6 +1,12 @@
-<div class="h-screen w-screen flex items-center justify-center">
+<script>
+  import ParticlesComponent from "$lib/Particles.svelte";
+</script>
+
+<ParticlesComponent />
+
+<div class="z-10 h-screen w-screen flex items-center justify-center">
   <div class="flex-col font-display text-center">
-    <div class="font-bold text-primary text-3xl md:text-5xl">
+    <div class="font-black text-primary text-3xl md:text-5xl">
       WELCOME
     </div>
     <div class="font-thin text-main md:text-2xl">
@@ -12,4 +18,3 @@
     </div>
   </div>
 </div>
-
